0455fb3125b57052f64e6e3bcac0702b58990e69
JSuzanne <56363435+Clawmer-FS@users.noreply.github.com>
2022-01-13 11:43:47 +0100

    Add files via upload

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jsuzanne <jsuzanne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:11:43 by jsuzanne          #+#    #+#             */
-/*   Updated: 2022/01/13 11:13:25 by jsuzanne         ###   ########.fr       */
+/*   Updated: 2022/01/13 11:43:28 by jsuzanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	//Move in 1 while the char is in set, save position
 	//Move to end and compare back to front the chars, save position
-	//Malloc (size * len - start - end + 1)
-	//copy 1 in new point from start to end add \0
 
 	int	start;
 	int	end;
 	char *res;
 
+	if (!s1)
+		return(NULL);
 	start = 0;
 	end = ft_strlen(s1) - 1;
 	while (s1[start] && to_cut(s1[start], *set))
 		start++;
 	while (s1[end] && to_cut(s1[end], *set))
 		end--;
+	return (ft_substr(s1, start, (end - start + 1)));
 }
